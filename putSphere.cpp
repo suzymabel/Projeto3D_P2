582742fd8722934b76ce7bf7e1ef002bd2bf6f2d
suzymabel <105361973+suzymabel@users.noreply.github.com>
2022-07-21 10:07:49 -0300

    Create putSphere.cpp

---
+++ putSphere.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include "putSphere.h"
+
+putSphere::putSphere(int xcenter, int ycenter, int zcenter, int radius, float r, float g, float b, float a){
+    this->xcenter = xcenter;
+    this->ycenter = ycenter;
+    this->zcenter = zcenter;
+    this->radius = radius;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
+
+}
+
+void putSphere::draw(Sculptor &s){
+  s.setColor(r, g, b, a);
+    for (int i = xcenter-radius; i <xcenter+radius; i++){
+      for (int j = ycenter-radius; j <ycenter+radius; j++){
+        for (int k = zcenter-radius; k <zcenter+radius; k++){
+          if((pow(i-xcenter,2) + pow(j-ycenter,2) + pow(k-zcenter, 2)) <= pow(radius, 2)){
+            s.putVoxel(i, j ,k);
+          }
+        }
+      }
+    }
+}
